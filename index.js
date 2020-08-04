af7f78340691047b51c5f06aea549190e5d5d16e
Dave North <dave.north@rewind.io>
2020-08-04 12:49:40 -0400

    Download using got

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const context = github.context;
+const request = require('request-promise');
 const { parseConfig, hasAssignee, getReviewers } = require("./lib/util");
 
 // most @actions toolkit packages have async methods
@@ -11,27 +12,36 @@ async function run() {
     const octokit = new github.GitHub(token);
     var configContent;
 
-    console.log(`Reading config file`);
+    console.log(`Attempting to read config from ${configPath}`);
 
     if (configPath.startsWith("http")) {
-      // Get the content
-      console.log(`config url`);
+      console.log(`Reading config from URL`);
+
+      try {
+        const response = await got(configPath);
+        console.log(response.body.url);
+        console.log(response.body.explanation);
+
+        configContent = response.body;
+      } catch (error) {
+        console.log(error.response.body);
+      }
     } else {
-      console.log(`local path`);
-      // configContent = await fetchContent(octokit, configPath);
+      console.log(`Reading config from local path`);
+      configContent = await fetchContent(octokit, configPath);
     }
 
-    //const config = parseConfig(configContent);
+    const config = parseConfig(configContent);
 
-    //core.debug("config");
-    //core.debug(JSON.stringify(config));
+    core.debug("config");
+    core.debug(JSON.stringify(config));
 
-    //const issuer = context.payload.pull_request.user.login;
+    const issuer = context.payload.pull_request.user.login;
 
-    //if (hasAssignee(config, issuer)) {
-    //  let reviewers = getReviewers(config, issuer);
-    //  assignReviewers(octokit, reviewers);
-    //}
+    if (hasAssignee(config, issuer)) {
+      let reviewers = getReviewers(config, issuer);
+      assignReviewers(octokit, reviewers);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
