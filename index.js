9b74b9e5cbec32c43063c9ca166b0aeba67fcd39
Dave North <dave.north@rewind.io>
2020-08-04 13:24:36 -0400

    This time

--- index.js
+++ index.js
@@ -6,25 +6,16 @@ const { parseConfig, hasAssignee, getReviewers } = require("./lib/util");
 
 // most @actions toolkit packages have async methods
 async function run() {
+  let configContent = '';
+
   try {
     const token = core.getInput("token", { required: true });
     const configPath = core.getInput("config");
     const octokit = new github.GitHub(token);
-    var configContent;
 
     if (configPath.startsWith("http")) {
       console.log(`Reading config from URL`);
-
-      (async () => {
-        try {
-            const response = await got(configPath);
-            console.log(response.body);
-            configContent = response.body;
-        } catch (error) {
-            console.log(error.response.body);
-            throw error;
-        }
-      })();
+      configContent = await getUrl(configPath);
     } else {
       console.log(`Reading config from local path`);
       configContent = await fetchContent(octokit, configPath);
@@ -64,6 +55,11 @@ async function fetchContent(client, repoPath) {
   });
 
   return Buffer.from(response.data.content, response.data.encoding).toString();
+}
+
+async function getUrl(url) {
+  const response = await got('url');
+  return Buffer.from(response.body).toString();
 }
 
 run();
